2f077c87b3f9b6a7c54e3fefe2ab3e65c23568e1
warbee0712 <warbee0712@outlook.com>
2017-02-02 14:45:00 -0200

    Defined basic app structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+
+import Calendar from './Calendar'
 
 class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      selectedDate: new Date()
+    }
+  }
+  selectDay (day) {
+    this.setState({
+      selectedDate: new Date(this.state.selectedDate.setDate(day))
+    })
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="App-title">Calendar</h1>
+        <Calendar
+          year={this.state.selectedDate.getFullYear()}
+          month={this.state.selectedDate.getMonth()}
+          day={this.state.selectedDate.getDate()}
+          tasks={[
+            {day: 25, body: 'Task 1'},
+            {day: 30, body: 'Task 2'}
+          ]}
+          selectDay={this.selectDay.bind(this)}
+        />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
